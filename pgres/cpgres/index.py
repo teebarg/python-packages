ed7cd8b6652d70cb689daa26fbca8e87a38e48aa
Adeniyi <teebarg01@gmail.com>
2024-05-17 13:44:19 +0100

    Update

--- pgres/cpgres/index.py
+++ pgres/cpgres/index.py
@@ -1,6 +1,6 @@
 def pgres_fuc():
-    print("pgres..................version 4.0.0")
+    print("pgres..................version 5.0.0")
 
 
 def sec_func():
-    print("pgres sec...........version 4.0.0")
+    print("pgres sec...........version 5.0.0")
